053cb75ca889f1aad47ba743e116a342070e1e74
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-06-04 01:04:19 +0700

    hotfix: change naming job

--- src/components/PredictForm.vue
+++ src/components/PredictForm.vue
@@ -120,7 +120,7 @@ async function submitForm() {
           <option value="Swasta">Swasta</option>
           <option value="Nelayan">Nelayan</option>
           <option value="Wiraswasta">Wiraswasta</option>
-          <option value="TNI/Polri">TNI/Polri</option>
+          <option value="TNI/POLRI">TNI/Polri</option>
           <option value="Tani">Petani</option>
           <option value="Buruh">Buruh</option>
           <option value="Lain-lain">Lainnya</option>
@@ -140,7 +140,7 @@ async function submitForm() {
           <option value="Swasta">Swasta</option>
           <option value="Nelayan">Nelayan</option>
           <option value="Wiraswasta">Wiraswasta</option>
-          <option value="TNI/Polri">TNI/Polri</option>
+          <option value="TNI/POLRI">TNI/Polri</option>
           <option value="Tani">Petani</option>
           <option value="Ibu Rumah Tangga">Ibu Rumah Tangga</option>
           <option value="Lain-lain">Lainnya</option>
